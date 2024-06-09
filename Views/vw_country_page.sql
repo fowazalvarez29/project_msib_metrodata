2c9fe8c5f7ae9729696ee4812ed2f359a02c767f
Syauqi-S <syauqisyafiq21@gmail.com>
2024-06-09 13:43:49 +0700

    Menambah beberapa view

---
+++ Views/vw_country_page.sql
@@ -0,0 +1,3 @@
+CREATE VIEW vw_country_page AS
+SELECT id, country_name, region_id
+FROM tbl_countries
---
+++ Views/vw_location_page.sql
@@ -0,0 +1,3 @@
+CREATE VIEW vw_location_page AS
+SELECT id, street_address, postal_code, city, state_province, country_id
+FROM tbl_locations
