3261f47480fef11f15fef6312e3278aa4d3f8979
Your Name <fowaz.213040008@mail.unpas.ac.id>
2024-06-16 13:10:48 +0700

    Memperbaiki Query

--- Fitur Tambahan/Presensi Jam Karyawan dan Bonus Overtime/Tabel Attendance dan Overtime.sql
+++ Fitur Tambahan/Presensi Jam Karyawan dan Bonus Overtime/Tabel Attendance dan Overtime.sql
@@ -6,7 +6,7 @@ CREATE TABLE tbl_attendance (
     check_in_status VARCHAR(10),
     check_out_status VARCHAR(10),
     PRIMARY KEY (employee_id, check_in),
-    CONSTRAINT FK_employee_attendance FOREIGN KEY (employee_id) REFERENCES tbl_employees (id)
+    FOREIGN KEY (employee_id) REFERENCES tbl_employees (id)
 );
 
 -- Membuat tabel lembur
@@ -14,5 +14,5 @@ CREATE TABLE tbl_overtime (
     employee_id INT NOT NULL,
     overtime_date DATE NOT NULL,
     PRIMARY KEY (employee_id, overtime_date),
-    CONSTRAINT FK_employee_overtime FOREIGN KEY (employee_id) REFERENCES tbl_employees (id)
+    FOREIGN KEY (employee_id) REFERENCES tbl_employees (id)
 );
