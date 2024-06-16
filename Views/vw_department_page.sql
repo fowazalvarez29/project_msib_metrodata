acc173e2475cf1bad3af6bea26ca54c014bfa03b
Syauqi-S <syauqisyafiq21@gmail.com>
2024-06-16 21:29:48 +0700

    Perbaiki view

--- Views/vw_department_page.sql
+++ Views/vw_department_page.sql
@@ -1,3 +1,4 @@
-CREATE VIEW vw_department_page AS
-SELECT id, department_name, location_id
-FROM tbl_departments
+CREATE OR ALTER VIEW vw_department_page AS
+SELECT d.id, department_name, city
+FROM tbl_departments d
+JOIN tbl_locations l ON d.location_id = l.id
