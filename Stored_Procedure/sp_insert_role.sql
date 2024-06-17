2357d7fc90570a193b88af57385437de4d343092
PutriAzizahNgoding <myputunity@gmail.com>
2024-06-17 21:29:07 +0700

    Revisi pada insert role, permission, region

--- Stored_Procedure/sp_insert_role.sql
+++ Stored_Procedure/sp_insert_role.sql
@@ -1,18 +1,22 @@
-USE Employee_Management_System;
+USE Employee_Management_System
+GO
+
+IF OBJECT_ID('sp_insert_role') IS NOT NULL
+DROP PROC sp_insert_role;
+GO
 
 -- STORED PROCEDURE INSERT ROLE
 CREATE PROCEDURE sp_insert_role 
-    @id INT,
     @role_name VARCHAR(50)
 AS
 BEGIN
-    INSERT INTO [dbo].[tbl_roles] ([id],[role_name])
-    VALUES (@id, @role_name);
+    INSERT INTO [dbo].[tbl_roles] ([role_name])
+    VALUES (@role_name);
 END
 GO
 
 -- Jalankan procedure
-EXEC sp_insert_role @id = 5, @role_name  = 'CS';
+EXEC sp_insert_role @role_name  = 'CS';
 
 SELECT * FROM tbl_roles;
 
